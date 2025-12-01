02d4f519494ef849744d35f68b81dc560f2e3871
namirso <douglasofc00@gmail.com>
2025-12-01 19:15:14 -0300

    mudanças finais - migration inserção dados

---
+++ sistema-review/src/main/resources/db/migration/V5__insert_dados.sql
@@ -0,0 +1,8 @@
+insert into usuarios (nome,email,senha,permissao) values ('ADMIN','admin@review','123',1);
+
+insert into usuarios (nome,email,senha,permissao) values ('teste','t@1','1',2);
+
+
+insert into tipos (nome) values ('Filme');
+insert into tipos (nome) values ('Série');
+
